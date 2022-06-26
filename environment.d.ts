274a4aaa6c0acbb6f7575005eb443d335ed861cd
Abhiek187 <achaudhuri2011@yahoo.com>
2022-06-25 22:13:50 -0400

    Create route to fetch recipes

--- environment.d.ts
+++ environment.d.ts
@@ -5,6 +5,8 @@ declare global {
     // https://stackoverflow.com/a/53981706
     interface ProcessEnv {
       PORT?: string;
+      APPLICATION_ID: string;
+      APPLICATION_KEY: string;
     }
   }
 }
--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
+import "dotenv/config"; // fetch secrets from .env
 import express from "express";
+
+import recipes from "./routes/recipes";
 
 const app = express();
 
 // Initialize middleware: parse JSON
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.status(200).send({
-    message: "Hello World!",
-  });
-});
+// Define routes
+app.use("/api/recipes", recipes);
 
 // parseInt() requires a string, not undefined
 const port = parseInt(`${process.env.PORT}`) || 5000;
