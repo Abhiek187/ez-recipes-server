37e1ad87a7dc03b25f9237d88f46c2d4cc734db0
Abhiek187 <achaudhuri2011@yahoo.com>
2022-07-10 19:35:48 -0400

    Make each nutrient a name property instead of a key

--- routes/recipes.ts
+++ routes/recipes.ts
@@ -27,21 +27,38 @@ router.get("/random", async (req, res) => {
 
     // Map the server-side response to the client-side schema
     const { nutrients, ingredients } = recipe.nutrition;
-    const calories = nutrients.find((nutrient) => nutrient.name === "Calories");
-    const fats = nutrients.find((nutrient) => nutrient.name === "Fat");
-    const saturatedFats = nutrients.find(
-      (nutrient) => nutrient.name === "Saturated Fat"
-    );
-    const carbs = nutrients.find(
-      (nutrient) => nutrient.name === "Carbohydrates"
-    );
-    const fiber = nutrients.find((nutrient) => nutrient.name === "Fiber");
-    const sugars = nutrients.find((nutrient) => nutrient.name === "Sugar");
-    const protein = nutrients.find((nutrient) => nutrient.name === "Protein");
-    const cholesterol = nutrients.find(
-      (nutrient) => nutrient.name === "Cholesterol"
-    );
-    const sodium = nutrients.find((nutrient) => nutrient.name === "Sodium");
+    // A list of all the nutrients to display in the client apps
+    const nutrientNames = [
+      "Calories",
+      "Fat",
+      "Saturated Fat",
+      "Carbohydrates",
+      "Fiber",
+      "Sugar",
+      "Protein",
+      "Cholesterol",
+      "Sodium",
+    ];
+
+    const filteredNutrients: {
+      name: string;
+      amount: number;
+      unit: string;
+    }[] = [];
+
+    for (const nutrientName of nutrientNames) {
+      const nutrient = nutrients.find(
+        (nutrient) => nutrient.name === nutrientName
+      );
+
+      if (nutrient !== undefined) {
+        filteredNutrients.push({
+          name: nutrient.name,
+          amount: nutrient.amount,
+          unit: nutrient.unit,
+        });
+      }
+    }
 
     const resJson: Recipe = {
       id: recipe.id,
@@ -54,44 +71,7 @@ router.get("/random", async (req, res) => {
       time: recipe.readyInMinutes,
       servings: recipe.servings,
       summary: recipe.summary,
-      nutrients: {
-        calories: {
-          amount: calories?.amount,
-          unit: calories?.unit,
-        },
-        fats: {
-          amount: fats?.amount,
-          unit: fats?.unit,
-        },
-        saturatedFats: {
-          amount: saturatedFats?.amount,
-          unit: saturatedFats?.unit,
-        },
-        carbs: {
-          amount: carbs?.amount,
-          unit: carbs?.unit,
-        },
-        fiber: {
-          amount: fiber?.amount,
-          unit: fiber?.unit,
-        },
-        sugars: {
-          amount: sugars?.amount,
-          unit: sugars?.unit,
-        },
-        protein: {
-          amount: protein?.amount,
-          unit: protein?.unit,
-        },
-        cholesterol: {
-          amount: cholesterol?.amount,
-          unit: cholesterol?.unit,
-        },
-        sodium: {
-          amount: sodium?.amount,
-          unit: sodium?.unit,
-        },
-      },
+      nutrients: filteredNutrients,
       // Ignore the nutrients information for each ingredient
       ingredients: ingredients.map((ingredient) => ({
         id: ingredient.id,
