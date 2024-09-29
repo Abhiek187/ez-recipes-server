0c3971ffac3bb1449c6e81baff57414b19eaf0f5
Abhiek187 <achaudhuri2011@yahoo.com>
2024-09-29 15:41:07 -0400

    Create key for testing crypto

--- tests/crypto.test.ts
+++ tests/crypto.test.ts
@@ -1,6 +1,18 @@
 import Encryptor from "../utils/crypto";
 
 describe("crypto", () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV, SECRET_KEY: "test" };
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    process.env = OLD_ENV;
+  });
+
   it.each(["", "test", "The quick brown fox jumps over the lazy dog."])(
     "should encrypt and decrypt: %s",
     (originalStr: string) => {
