89c53ba7a1b632b32c5c6072da3ec1a6ea7a8805
Abhiek187 <achaudhuri2011@yahoo.com>
2025-02-04 21:24:57 -0500

    Add certain headers to the logs

--- middleware/logger.ts
+++ middleware/logger.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import { redactHeadersAndFields } from "../utils/axios";
+import { filterObject } from "../utils/object";
 
 const logger = (req: Request, _res: Response, next: NextFunction) => {
   // Log all incoming requests
   const { method, originalUrl, headers, body } = req;
-  // Logging all headers isn't needed, but may change later
   const { dataForLogging } = redactHeadersAndFields(headers, body);
+  // Only log the most useful headers to differentiate users
+  const filteredHeaders = filterObject(headers, [
+    "user-agent",
+    "accept-language",
+    "origin",
+  ]);
 
-  let log = `[Incoming Request] ${new Date()} | Method: ${method} | URL: ${originalUrl}`;
+  let log =
+    `[Incoming Request] ${new Date()} | Method: ${method} | URL: ${originalUrl} | ` +
+    `Headers: ${JSON.stringify(filteredHeaders)}`;
   // Don't log empty request bodies
   if (dataForLogging !== undefined) {
     log += ` | Data: ${JSON.stringify(dataForLogging)}`;
